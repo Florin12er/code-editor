4aa040d08ac17183b6b677ae9d37127c95d2de55
Florin12er <apetreiflorin236@gmail.com>
2024-08-02 03:59:43 +0200

    added the componets

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,22 +6,22 @@ import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Code editor",
-  description: "this is a simple code editor",
+    title: "Code editor",
+    description: "this is a simple code editor",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <>
-      <ClerkProvider>
-        <html lang="en">
-          <body className={inter.className}>{children}</body>
-        </html>
-      </ClerkProvider>
-    </>
-  );
+    return (
+        <>
+            <ClerkProvider>
+                <html lang="en">
+                    <body className={inter.className}>{children}</body>
+                </html>
+            </ClerkProvider>
+        </>
+    );
 }
